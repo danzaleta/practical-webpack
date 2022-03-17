d8df33f4d1b0206c35787c20ab0a995b68b2676d
Daniel Zaleta <zaletadaniel@gmail.com>
2022-03-16 21:34:21 -0600

    Stylus implemented

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,11 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                test: /\.css|.styl$/i,
+                use: [MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ],
             }
         ]
     },
